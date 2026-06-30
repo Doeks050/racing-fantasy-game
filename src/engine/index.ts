49683fb3c799d9c9e03f34c49e2838dfa9842a32
Doeks050 <nicktalens@outlook.com>
2026-06-30 13:10:14 +0200

    Export MarketEngine

--- src/engine/index.ts
+++ src/engine/index.ts
@@ -1,5 +1,6 @@
 export * from "./GameState";
 export * from "./InventoryEngine";
 export * from "./LoadoutEngine";
+export * from "./MarketEngine";
 export * from "./RaceEngine";
 export * from "./SaveEngine";
